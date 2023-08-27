645a6fe397001a632a777e1cdce56bbf6052b466
Lasantha Sanjeewa Silva <41699526+sanju2@users.noreply.github.com>
2023-08-27 21:22:03 +0400

    Initial Commit

--- application-code/app-tier/DbConfig.js
+++ application-code/app-tier/DbConfig.js
@@ -1,6 +1,6 @@
 module.exports = Object.freeze({
-    DB_HOST : '',
-    DB_USER : '',
-    DB_PWD : '',
-    DB_DATABASE : ''
+    DB_HOST : 'database-1-instance-1.cfvufltihmhv.us-east-2.rds.amazonaws.com',
+    DB_USER : 'admin',
+    DB_PWD : 'Pa$$w0rd1234',
+    DB_DATABASE : 'webappdb'
 });
